13c31ae0b86bdace27d87b7eef85c65b8ea133b9
nasar <nasarroble@gmail.com>
2015-05-25 13:38:37 -0500

    playing with the view

--- app/src/androidTest/java/com/example/nasar/mastermindgame/MasterMindGameTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.nasar.mastermindgame;
-
-/**
- * Created by nasar on 5/25/15.
- */
-public class MasterMindGameTest {
-}
--- app/src/main/java/com/example/nasar/mastermindgame/MainActivity.java
+++ app/src/main/java/com/example/nasar/mastermindgame/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.nasar.mastermindgame;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addRow();
+
+        createSpinners((Spinner) findViewById(R.id.colors1));
+        createSpinners((Spinner) findViewById(R.id.colors2));
+        createSpinners((Spinner) findViewById(R.id.colors3));
+        createSpinners((Spinner) findViewById(R.id.colors4));
+
+
     }
 
     @Override
@@ -34,5 +47,25 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createSpinners(Spinner spinner) {
+
+        Spinner spin = spinner;
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.colors_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spin.setAdapter(adapter);
+
+    }
+
+    public void addRow() {
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.table1);
+        TableRow row = new TableRow(this);
+
+        tableLayout.addView(row);
     }
 }
